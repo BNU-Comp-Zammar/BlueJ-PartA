433896967618ff91698345fbdadea0d819462ef5
M Zammar Baig <22011339@bucks.ac.uk>
2020-10-15 13:50:59 +0100

    replaced course

--- app01-lab-classes/Course.java
+++ app01-lab-classes/Course.java
@@ -2,21 +2,30 @@
 /**
  * Write a description of class Course here.
  *
- * @author Derek Peacock
+ * @author Zammar Baig
  * @version 0.1
  */
 public class Course
 {
+    // Attribute, Field, Varible 
     private String title;
     
-    private String code;
+    private String codeNo;
+    
+    //Methods
+    
 
     /**
      * Constructor for objects of class Course
      */
-    public Course()
+    public Course(String title, String code)
     {
-        
+        this.title = title;
+        this.codeNo = codeNo;
+    } 
+    
+    public  void print()
+    {
+        System.out.println("Couerse: " + title + ", Code No: " + codeNo);
     }
-
 }
