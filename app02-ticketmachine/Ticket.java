437cf95ec9f9e8c82df7d3f43775221ecfdf4973
M Zammar Baig <22011339@bucks.ac.uk>
2020-10-19 14:37:14 +0100

    added destination and date to ticket

---
+++ app02-ticketmachine/Ticket.java
@@ -0,0 +1,27 @@
+import java.util.Date;
+
+/**
+ * Write a description of class Ticket here.
+ *
+ * @author (Zammar Baig)
+ * @version (0.1 - Monday 19th )
+ */
+public class Ticket
+{
+    //Journey Destination 
+    private String destination;
+    
+    //This is the ticket in pence
+    private int price;
+    
+    //This is the date and time the ticket was printed
+    private Date timeStamp;
+    
+    
+    /**
+     * Constructor for objects of class Ticket
+     */
+    public Ticket()
+    {
+    }
+}
