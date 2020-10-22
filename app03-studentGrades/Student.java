3d94aa9cc8ba9dfb2b95954d9381972b11f8492c
M Zammar Baig <22011339@bucks.ac.uk>
2020-10-22 14:45:30 +0100

    added enroll method

--- app03-studentGrades/Student.java
+++ app03-studentGrades/Student.java
@@ -16,6 +16,8 @@ public class Student
     // the amount of credits for study taken so far
     private int credits;
     
+    private Course course; 
+    
     /**
      * Create a new student with a given name and ID number.
      */
@@ -84,4 +86,14 @@ public class Student
     {
         System.out.println(name + ", student ID: " + id + ", credits: " + credits);
     }
+    
+    /**
+     * this method will be used to enroll a student or students to a course
+     */
+    public void enrollOnCourse(Course course)
+    {
+        this.course = course;
+    }
+    
+    
 }
