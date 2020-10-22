fafad781cd3f39627f4e48a5607dedbb29ea6692
M Zammar Baig <22011339@bucks.ac.uk>
2020-10-22 15:06:52 +0100

    finished contructor

--- app03-studentGrades/Module.java
+++ app03-studentGrades/Module.java
@@ -2,16 +2,26 @@
 /**
  * Write a description of class Module here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (ZAMMAR BAIG)
+ * @version (version1)
  */
 public class Module
 {
+    //Fields
+    private String title; 
+    
+    private String codeNo; 
+    
+    private int mark; 
+    
     /**
      * Constructor for objects of class Module
      */
-    public Module()
+    public Module(String title, String codeNo)
     {
+        this.title = title;
+        this.codeNo = codeNo;
+        mark = 0;
     }
 
 
