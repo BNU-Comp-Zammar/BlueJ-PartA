feeac5e938ec7f744306b2f248e6331ec435e2b6
M Zammar Baig <22011339@bucks.ac.uk>
2020-10-26 14:05:59 +0000

    added final mark, final grade and create modules class

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -1,6 +1,6 @@
 
 /**
- * Write a description of class Course here.
+ * 
  *
  * @author Zammar Baig
  * @version 0.1
@@ -19,6 +19,10 @@ public class Course
     private Module module3;
     private Module module4;
     
+    private int averageMark;
+    
+    private Grades grade;
+    
     /**
      * Constructor for objects of class Course
      */
@@ -28,6 +32,10 @@ public class Course
         this.codeNo = codeNo;
         
         createModules();
+        module1 = new Module ("Programming Concepts", "CO452");
+        module2 = new Module ("Computing", "G100");
+        module3 = new Module ("Global Business", "BM454");
+        module4 = new Module ("Web Development", "CO454");
     } 
     
     public void createModules()
@@ -73,9 +81,39 @@ public class Course
     
     public void awardmark(int mark, int moduleNo)
     {
-        if(moduleNo ==1)
+        if(moduleNo == 1)
         {
             module1.awardMark(mark);
+        }
+        
+        if (moduleNo == 2)
+        {
+            module2.awardMark(mark);
+        }
+        
+        if (moduleNo == 3)
+        {
+            module3.awardMark(mark);
+        }
+        
+        if (moduleNo == 4)
+        {
+            module4.awardMark(mark);
+        }
+    }
+    
+    public void calculateFinalMark()
+    {
+        averageMark = module1.getMark() + module2.getMark() + module3.getMark()
+            + module4.getMark();
+        averageMark = averageMark / 4;
+    }
+    
+    public void calculateGrade()
+    {
+        if(averageMark >= 0 & averageMark < 40)
+        {
+            grade = Grades.F;
         }
     }
 }
