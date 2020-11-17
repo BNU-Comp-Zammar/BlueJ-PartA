78438180bb25c910bd37e9cede54b4276dfb8ee7
M Zammar Baig <22011339@bucks.ac.uk>
2020-11-17 18:42:56 +0000

    updated name

--- app05-stock/InputReader.java
+++ app05-stock/InputReader.java
@@ -1,12 +1,11 @@
-import java.util.HashSet;
 import java.util.Scanner;
 
 /**
  * InputReader reads typed text input from the standard text terminal. 
  * The text typed by a user is returned.
  * 
- * @author     Michael Kölling and David J. Barnes
- * @version    0.1 (2016.02.29)
+ * @author     Zammar Baig
+ * @version    1
  */
 public class InputReader
 {
