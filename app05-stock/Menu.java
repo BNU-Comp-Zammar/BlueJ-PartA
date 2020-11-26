7e1ef0a5da80ea47a40738fdd83eaf3d9cf430d8
M Zammar Baig <22011339@bucks.ac.uk>
2020-11-26 14:59:08 +0000

    added input reader
    edited name and version
    
    added inputed reader and used from stock manager

--- app05-stock/Menu.java
+++ app05-stock/Menu.java
@@ -2,14 +2,31 @@
 /**
  * Write a description of class Menu here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (zammar)
+ * @version 1
  */
 public class Menu
 {
-    public String getMenuChoice()
+    private InputReader reader = new InputReader();
+    
+    /**
+     * Display all the valid choices and make sure that the user 
+     * chosen before returning
+     */
+    public String getMenuChoice(String [] choices)
     {
-        return null;
+        boolean finished = false;
+        String choice = null ;
+        
+        while(!finished)
+        {
+            printChoices();
+           
+            choice = reader.getInput();
+            choice = choice.toLowerCase();
+            
+        }
+        return choice;
     }
     
     private void printChoices()
