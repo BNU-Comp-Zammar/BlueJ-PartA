4940da339f251f1f6f185d90042fdfc58d5265f2
M Zammar Baig <22011339@bucks.ac.uk>
2020-11-26 14:43:40 +0000

    created menu class

---
+++ app05-stock/Menu.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Menu here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Menu
+{
+    public String getMenuChoice()
+    {
+        return null;
+    }
+    
+    private void printChoices()
+    {
+    }
+}
