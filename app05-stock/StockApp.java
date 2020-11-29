1d2d9a9ba93917b2720e918a820815778846c4f1
M Zammar Baig <22011339@bucks.ac.uk>
2020-11-29 21:54:09 +0000

    clustered add product method
    used isEmpty

--- app05-stock/StockApp.java
+++ app05-stock/StockApp.java
@@ -11,18 +11,18 @@ public class StockApp
 {
     public final int FIRST_ID = 200;
     public final String ADD = "add";
-    
+
     // Use to get user input
     private InputReader input;
-    
+
     private StockManager manager = new StockManager();
-    
+
     private StockDemo demo;
-    
+
     private int nextID = FIRST_ID;
-    
+
     private String [] menuChoices;
-    
+
     /**
      * Constructor for objects of class StockApp
      */
@@ -31,10 +31,10 @@ public class StockApp
         input = new InputReader();
         manager = new StockManager();
         demo = new StockDemo(manager);
-        
+
         setUpMenu();
     }
-    
+
     private void setUpMenu()
     {
         menuChoices = new String []
@@ -50,26 +50,25 @@ public class StockApp
             "Quit the program"
         };
     }
-    
+
     /**
      * 
      */
     public void run()
     {
         boolean finished = false;
-        
+
         while(!finished)
         {
             printHeading();
-            
+
             String choice = Menu.getMenuChoice(menuChoices);
             executeMenuChoice(choice);
-            
+
             if(choice.startsWith("quit"))
                 finished = true;
         }
     }
-    
 
     /**
      * 
@@ -78,127 +77,133 @@ public class StockApp
     {
         if(choice.startsWith(ADD))
         {
-           addProduct();
+            addProduct();
         }
         else if(choice.startsWith("remove"))
         {
-           removeProducts();
+            removeProducts();
         }
         else if(choice.startsWith("print"))
         {
-           printAllProducts();
+            printAllProducts();
         }
         else if(choice.startsWith("rename"))
         {
-           renameProduct();
+            renameProduct();
         }
         else if(choice.startsWith("sell"))
         {
-           sellProduct();
+            sellProduct();
         }
         else if(choice.startsWith("deliver"))
         {
-           deliverProduct();
+            deliverProduct();
         }
         else if(choice.startsWith("low"))
         {
-           printLowStock();
+            printLowStock();
         }
         else if(choice.startsWith("restock"))
         {
-           restock();
+            restock();
         }
     }
-    
+
     public void addProduct()
     {
         System.out.println("Add a new Product");
         System.out.println();
-        
+
         System.out.println("Please enter the name of the product");
         String name = input.getInput();
-        
-        boolean isDuplicate = manager. isDuplicateID(nextID);
-        if(isDuplicate)
+
+        if(name.isEmpty())
         {
-            boolean finished = false;
-            
-            while(!finished)
+            System.out.println("You have entered a blank name");
+        }
+        else
+        {
+            boolean isDuplicate = manager. isDuplicateID(nextID);
+            if(isDuplicate)
             {
-                nextID++;
-                if(manager.isDuplicateID(nextID))
+                boolean finished = false;
+
+                while(!finished)
                 {
-                    finished = true;
+                    nextID++;
+                    if(manager.isDuplicateID(nextID))
+                    {
+                        finished = true;
+                    }
                 }
             }
+
+            Product product = new Product(nextID, name);
+            manager.addProduct(product);
+
+            System.out.println("/nAdded " + product + " to the stock\n");
+            nextID++; 
         }
-        
-        Product product = new Product(nextID, name);
-        manager.addProduct(product);
-        
-        System.out.println("\nAdded " + product + " to the stock\n");
-        nextID++; 
-        
+
     }
-    
+
     public void removeProducts()
     {
         System.out.println("Remove an old Product");
         System.out.println();
-        
+
         System.out.println("Please enter the id of the product");
         String number = input.getInput();
-        
+
         int id = Integer.parseInt(number);
         manager.removeProduct(id);
     }   
-    
+
     public void renameProduct()
     {
         System.out.println("Rename existing product");
         System.out.println();
-  
+
         int id = input.getInt("Please enter the id of the product");
         manager.removeProduct(id);
     }
-    
+
     public void restock()
     {
         int stockLevel = input.getInt("Please enter low stock level");
         int restockLevel = input.getInt("Please enter the restock level");
         manager.printLowStock(stockLevel, restockLevel);
     }
-    
+
     public void sellProduct()
     {
         System.out.println("Sell an existing product");
         System.out.println();
-  
+
         int id = input.getInt("Please enter the id of the product");
         int quantity = input.getInt("Please enter the quantity of the product");
         manager.sellProduct(id,quantity);
-       
+
     }
-    
+
     public void deliverProduct()
     {
         System.out.println("Enter ID of product for deliver");
         System.out.println();
     }
-    
+
     public void printLowStock()
     {
         int stockLevel = input.getInt("Please enter low stock level");
-        
-        
+
         manager.printLowStock(stockLevel, 0);
     }
-    
+
     public void printAllProducts()
     {
         manager.printAllProducts();
     }
-    
+
     /**
      * Print the title of the program and the authors name
      */
